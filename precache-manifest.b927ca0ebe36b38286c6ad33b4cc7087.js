dbcc07713245f15fb8d1a45119a765256da99df4
Januar Andaria <janucaria@gmail.com>
2019-04-04 23:35:58 +0800

    Updates

---
+++ precache-manifest.b927ca0ebe36b38286c6ad33b4cc7087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "109016bebed6a55264bb",
+    "url": "/chemdoodle-web-react-ts/static/css/main.b2fe3bac.chunk.css"
+  },
+  {
+    "revision": "109016bebed6a55264bb",
+    "url": "/chemdoodle-web-react-ts/static/js/main.3c9d5f7e.chunk.js"
+  },
+  {
+    "revision": "c397ec27bef18b881b15",
+    "url": "/chemdoodle-web-react-ts/static/js/runtime~main.fe703150.js"
+  },
+  {
+    "revision": "1c709dffabad6bb29fe1",
+    "url": "/chemdoodle-web-react-ts/static/js/2.fc717401.chunk.js"
+  },
+  {
+    "revision": "fcd492e3c0f5dc769c5dd38133d4913f",
+    "url": "/chemdoodle-web-react-ts/index.html"
+  }
+];
